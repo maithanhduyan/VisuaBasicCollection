646cb88dd62d8f7041db4508af738ebb964562e8
Mai Thành Duy An <tiachop0102@gmail.com>
2019-01-05 01:56:45 +0700

    Create Project EnumDemo

---
+++ VisuaBasicCollection/EnumDemo/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+    Public Enum Temperature
+        Low
+        Medium
+        High
+    End Enum
+    Sub Main()
+        Dim value As Temperature = Temperature.Medium
+        Select Case value
+            Case Temperature.Low
+                Console.WriteLine("Low Temperature")
+                Exit Select
+            Case Temperature.Medium
+                Console.WriteLine("Mediuam Temperature")
+                Exit Select
+            Case Temperature.High
+                Console.WriteLine("High Temperature")
+                Exit Select
+        End Select
+        Console.ReadLine()
+    End Sub
+
+End Module
